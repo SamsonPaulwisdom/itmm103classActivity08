80919059a61623fd31684a5154a9a495b279903c
Paul Wisdom <itmm103samsonpaulwisdom@gmail.com>
2018-07-24 23:39:54 +0800

    commitment number 5

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
 <head>
@@ -89,7 +88,7 @@ if (empty($_POST['password'])) {
 
 ?>
 
-<form method="post" action="insert.php"> 
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
   
   Username: <input type="text" name="username">
   <?php echo $usernameErr; ?>
@@ -123,91 +122,6 @@ if (empty($_POST['password'])) {
 
 
 
-=======
-<!DOCTYPE HTML>  
-<html>
-<head>
-<style>
-.error {color: #FF0000;}
-</style>
-</head>
-<body>  
-
-<?php
-// define variables and set to empty values
-$nameErr = $nameErr1 = $emailErr = $genderErr = $websiteErr = "";
-$name = $name1 = $email = $gender = $comment = $website = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "First name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed";
-    }
-  }
-
-
-   if (empty($_POST["lastname"])) {
-    $nameErr1 = "Last name is required";
-  } else {
-    $name1 = test_input($_POST["lastname"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$name1)) {
-      $nameErr1 = "Only letters and white space allowed";
-    }
-  }
-  
-
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format";
-    }
-  }
-
-}
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-?>
-
-<h2>PHP Form Validation Example</h2>
-<p><span class="error">* required field</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  First name: <input type="text" name="firstname" value="<?php echo $name;?>">
-  <span class="error">* <?php echo $nameErr;?></span>
-  <br><br>
-  Last Name: <input type="text" name="lastname" value="<?php echo $name1;?>">
-  <span class="error">* <?php echo $nameErr1;?></span>
-  <br><br>
-  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-  <span class="error">* <?php echo $emailErr;?></span>
-  <br><br>
-  <input type="submit" name="submit" value="Submit">  
-</form>
-
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $website;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $gender;
-?>
->>>>>>> 4fb260a40f22e3975bc8698e5146eb9748dcbe31
 
 </body>
 </html>
